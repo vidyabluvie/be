2f8880bbd068a248afb4516e91cd558e1e633851
vidyabluvie <43338120+vidyabluvie@users.noreply.github.com>
2018-09-18 11:58:02 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int n=3;
+void main()
+{
+clrscr();
+if(n>0)
+{
+printf("\nPositive");
+}
+else if(n<0)
+{
+printf("\nNegative");
+}
+else
+{
+printf("\nZero");
+}
+getch();
+}
